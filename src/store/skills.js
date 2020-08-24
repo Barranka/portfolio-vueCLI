3d095fab0e7b33004f2eeba936ac6804675a57f2
Barranka <cat123_93@mail.ru>
2020-08-24 18:51:14 +0300

    change img

--- src/store/skills.js
+++ src/store/skills.js
@@ -1,9 +1,9 @@
 export default {
 	state: {
 		skills: {
-			frontend: 'VUE JS, Nuxt.js, Axios, JavaScript, ES6, Node.js, JSON, AJAX, SASS, CSS, HTML, BEM ',
+			frontend: 'VUE JS, Nuxt.js, Axios, Vuex, JavaScript, ES6, Node.js, JSON, AJAX, SASS, CSS, HTML, BEM, ElementUI, Vuetify',
 			assembly: 'VUE CLI, Nuxt',
-			other: '1C-Bitrix, HostCMS, Photoshop, English language (pre-intermediate)'
+			other: 'Git, 1C-Bitrix, HostCMS, Photoshop, English language (pre-intermediate)'
 		}
 	}
 }
